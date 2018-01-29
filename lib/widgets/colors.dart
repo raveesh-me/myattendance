66cda2b11550162da90c3c84dda1a805c226fd2c
Raveesh Agarwal <raveeshr503@gmail.com>
2018-01-29 06:22:02 +0530

    App logic.. complete!! The app is fully functional. Writing tests for getMapNumber method.

---
+++ lib/widgets/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+Color themePrimaryColor = Colors.brown;
+Color themeAccentColor = Colors.deepOrangeAccent;
+Color bigCardBackground = Colors.blueGrey;
+Color bigCardBigText = Colors.white;
+Color bigCardSmallText = Colors.white;
+Color smallCardBigText = Colors.white;
+Color smallCardSmallText = Colors.white;
